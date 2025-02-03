696d0607bedcea8bfd0ad4ee7a35e9cbbbccd8af
Abir <abirgpx@gmail.com>
2025-02-03 11:43:21 +0600

    hi

---
+++ check age.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age : " ))
+
+if age < 10 :
+    print ("You are too young for this class")
+    
+elif age < 20 :
+    print ("You are too old for this class")
+    
+else :
+    print ("You can do this class")
--- medical condition.py
+++ medical condition.py
@@ -3,11 +3,9 @@ medical = input("did you have a medical cause Y or N:")
 attendance = int(input("Enter the attendance of the student:"))
 
 if medical == 'Y' or medical == "y":
-    print ("You are Allowed")
-    
+    print ("You are Allowed") 
 else:
     if attendance >= 75:
         print("Allowed")
-        
     else:
         print("Not Allowed")
